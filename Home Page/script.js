3c3279c590447efb3f2937ff102a043928f121ab
DeepayuNinama <138458194+DeepayuNinama@users.noreply.github.com>
2024-07-02 23:02:38 +0530

    new

--- Home Page/script.js
+++ Home Page/script.js
@@ -1,5 +1,5 @@
 let text = document.getElementById('text');
-let leaf = document.getElementById('leaf');
+// let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill3 = document.getElementById('hill3');
@@ -10,8 +10,8 @@ window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     text.style.marginTop = value * 2.5 + 'px';
-    leaf.style.top = value * -1.5 +'px';
-    leaf.style.left = value * 1.5 +'px';
+    // leaf.style.top = value * -1.5 +'px';
+    // leaf.style.left = value * 1.5 +'px';
     hill5.style.left = value * 1 +'px';
 
     hill3.style.left = value * 1.5 +'px';
