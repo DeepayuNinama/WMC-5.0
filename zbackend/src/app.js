cb75c377f678ffc087e0fa71e93a5e037f87c74a
DeepayuNinama <138458194+DeepayuNinama@users.noreply.github.com>
2024-07-28 20:45:15 +0530

    .

--- zbackend/src/app.js
+++ zbackend/src/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
@@ -8,7 +10,6 @@ const User = require("./models/User");
 const hbs = require('hbs');
 
 const paypal = require('paypal-rest-sdk');
-require('dotenv').config();
 
 const app = express();
 const mongoURL = "mongodb://127.0.0.1:27017/GTA5";
@@ -75,6 +76,7 @@ const templates_path = path.join(__dirname, "../templates/views");
 
 // Serve static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 paypal.configure({
   'mode': process.env.PAYPAL_MODE,
